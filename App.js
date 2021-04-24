58f0ea05c04108e6a08c3be35e38197679e41ddc
Michael Papesca <michael.papesca@onmail.com>
2021-04-23 19:08:31 -0600

    Add firebase config initialization

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import keys from './config/keys';
+
+// Initialize firebase
+firebase.initializeApp(keys.firebaseConfig);
 
 export default function App() {
   return (
